adae1aa9f5592011830e1543c13e91a5a4e2a7af
Rowena Lohman <rolohman@gmail.com>
2025-03-05 14:26:42 -0500

    soilscape

--- setParams.py
+++ setParams.py
@@ -1,19 +1,23 @@
 import datetime
 
 
-mainPath      = '/Users/rlohman/SMCalVal/'
-sparseTypes   = ('SCAN','NEON')
-superTypes    = ('SOILSCAPE')
-outFile       = mainPath+'results.csv'
+mainPath      = '/Users/rlohman/Desktop/SMCalVal/'
+outFileTot    = mainPath+'results.csv'
+
+
 scanPath      = mainPath+'SCAN/'
 neonPath      = mainPath+'NEON/'
-soilPath      = mainPath+'SOILSCAPE/'
+soilscapePath = mainPath+'SOILSCAPE/'
 
 ##move these files from supportFiles/
-anciStatic    = '/Users/rlohman/Software/SMCalVal/supportFiles/NISAR_SM_STATIC_ANCILLARY_002.h5'
-trackFrameDb  = mainPath+'NISAR_TrackFrame_L_20240530.gpkg' 
-soilMeta      = soilPath+'soilscape_site_meta_data.csv'
-neonDepths    = neonPath+'swc_depthsV2.csv'    
+##General
+anciStatic    = mainPath+'supportFiles/NISAR_SM_STATIC_ANCILLARY_002.h5'
+trackFrameDb  = mainPath+'supportFiles/NISAR_TrackFrame_L_20240530.gpkg' 
+##Soilscape
+soilscapeMeta = mainPath+'supportFiles/SOILSCAPE/soilscape_site_meta_data.csv'
+soilscapeNode = mainPath+'supportFiles/SOILSCAPE/soilscape_site_nodeLocations.csv'
+#NEON
+neonDepths    = mainPath+'supportFiles/NEON/swc_depthsV2.csv'    
 
 
 # Set these to initial target timespan - here only used to exclude sensors with no overlap with this timespan
