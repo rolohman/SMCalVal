0b17de437848c4d6652a82897966ca323a798384
Rowena Lohman <rolohman@gmail.com>
2025-03-17 15:55:56 -0400

    Marchanges

--- setParams.py
+++ setParams.py
@@ -14,11 +14,15 @@ soilscapePath = mainPath+'SOILSCAPE/'
 anciStatic    = mainPath+'supportFiles/NISAR_SM_STATIC_ANCILLARY_002.h5'
 trackFrameDb  = mainPath+'supportFiles/NISAR_TrackFrame_L_20240530.gpkg' 
 ##Soilscape
-soilscapeMeta = mainPath+'supportFiles/SOILSCAPE/soilscape_site_meta_data.csv'
+#soilscapeMeta = mainPath+'supportFiles/SOILSCAPE/soilscape_site_meta_data.csv'
 soilscapeNode = mainPath+'supportFiles/SOILSCAPE/soilscape_site_nodeLocations.csv'
 #NEON
 neonDepths    = mainPath+'supportFiles/NEON/swc_depthsV2.csv'    
 
+
+#output files
+#Soilscape
+soilscapeSitesPath = soilscapePath+'SOILSCAPESites.csv'
 
 # Set these to initial target timespan - here only used to exclude sensors with no overlap with this timespan
 startDate     = datetime.datetime(2021,4,1)
--- utilsCalVal.py
+++ utilsCalVal.py
@@ -1,5 +1,10 @@
-from osgeo import gdal,osr
-
+from osgeo import osr
+import numpy as np
+import glob
+import re
+import os
+import datetime
+from datetime import timedelta
 
 def EASEconvert(x,y,LatLon): 
     #LatLon=1 go from LatLon to EASE2: x = lon, y=lat
@@ -34,4 +39,51 @@ def EASEconvert(x,y,LatLon):
         x           = coords[1] #lon
         y           = coords[0] #lat
     return x,y
-  
+
+def read_WalnutGulch():
+
+    workDir      = '/scratch/rlohman/'
+    retDir       = workDir+'R4/'
+    outFiles     = np.array(glob.glob(retDir+('[0-9]'*8)+'.h5'))
+
+    tmp          = re.search(r'2[0-9]{7}',outFiles[0])
+    inds         = tmp.span()
+    dates        = np.array([datetime.datetime.strptime(x[inds[0]:inds[1]],'%Y%m%d') for x in outFiles])
+
+    sort_index   = np.argsort(dates)
+    gcovs        = outFiles[sort_index]
+    dates        = dates[sort_index]
+
+    allgcovs      = np.reshape([dirName+os.path.basename(x) for dirName in retDir for x in gcovs],[len(modDirs),len(dates)])
+
+
+
+
+    idx = np.array([np.linalg.norm(x) for x in eci-ptezc]).argmin()
+    idy = np.array([np.linalg.norm(x) for x in eri-ptezr]).argmin()
+    print(lon[idx],lat[idy],ptezr,ptezc,idx,idy)
+    retr= np.zeros([len(modDirs),len(gcovs),3])*np.nan
+    rete= np.zeros([len(modDirs),len(gcovs),3])*np.nan
+    HH  = np.zeros([len(modDirs),len(gcovs)])*np.nan
+    mods=('DSG','PMI','TSR')
+    for j in range(len(modDirs)):
+        for i in range(len(dates)):
+                if os.path.isfile(allgcovs[j,i]):
+                    fo=h5py.File(allgcovs[j,i],'r')
+                    dataset='/science/LSAR/SME2/grids/radarData/frequencyA/sigma0HH'
+                    if dataset in fo:
+                        HH[j,i] =fo[dataset][idy,idx]
+
+                    for k in range(len(mods)):
+                        dataset='/science/LSAR/SME2/grids/algorithmCandidates/'+mods[k]+'/soilMoisture'
+                        if dataset in fo:
+                            retr[j,i,k] =fo[dataset][idy,idx]
+                    for k in range(len(mods)):
+                        dataset='/science/LSAR/SME2/grids/algorithmCandidates/'+mods[k]+'/soilMoistureUncertainty'
+                        if dataset in fo:
+                            rete[j,i,k] =fo[dataset][idy,idx]
+                    
+                    fo.close()
+    retr[retr<0] = np.nan
+    rete[rete<0] = np.nan
+    HH[HH<0]     = np.nan
